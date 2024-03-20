29ddef5bdf6b3802816946bfb645a67310d8324f
dora <159266461+dorabonaldo@users.noreply.github.com>
2024-03-20 10:02:42 -0300

    Add files via upload

---
+++ Aula.py
@@ -0,0 +1,106 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+isa
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    isa
+NameError: name 'isa' is not defined
+"isa"
+'isa'
+nome
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    nome
+NameError: name 'nome' is not defined. Did you mean: 'None'?
+nome
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    nome
+NameError: name 'nome' is not defined. Did you mean: 'None'?
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+isa
+numero
+16
+booleano
+True
+nome
+'isa'
+
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+isa
+a media é 8.0
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é <built-in function pow>
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+Traceback (most recent call last):
+  File "C:/Users/202102584027/Desktop/trabalhos/exercício.py", line 22, in <module>
+    palavra = ultravioleta
+NameError: name 'ultravioleta' is not defined
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+Traceback (most recent call last):
+  File "C:/Users/202102584027/Desktop/trabalhos/exercício.py", line 22, in <module>
+    palavra = ultravioleta
+NameError: name 'ultravioleta' is not defined
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+a quantidade de letras na palavra é 12
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+a quantidade de letras na palavra é 12
+Traceback (most recent call last):
+  File "C:/Users/202102584027/Desktop/trabalhos/exercício.py", line 28, in <module>
+    for i in range(0, len(string)): "essa sala está coneglando"
+NameError: name 'string' is not defined
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+a quantidade de letras na palavra é 12
+Traceback (most recent call last):
+  File "C:/Users/202102584027/Desktop/trabalhos/exercício.py", line 28, in <module>
+    for i in range(0, len(string)): "essa sala está coneglando"
+NameError: name 'string' is not defined
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+a quantidade de letras na palavra é 12
+a quantidade de espaços em branco da frase é 3
+
+========= RESTART: C:/Users/202102584027/Desktop/trabalhos/exercício.py ========
+a media é 8.0
+14 ao quadrado é 196
+o dobro de um dos número é 40
+a quantidade de letras na palavra é 12
+a quantidade de espaços em branco da frase é 3
+o primeiro número é maior que o segundo? False
+o maior número é 14
